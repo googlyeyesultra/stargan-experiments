18590d72513cc571ea61dab9548a3207bda02e85
googlyeyesultra <googlyeyesultra@gmail.com>
2024-07-31 12:57:37 -0500

    Initial spectral norm for discriminator implementation.

--- model.py
+++ model.py
@@ -2,6 +2,7 @@ import torch
 import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
+from torch.nn.utils.parametrizations import spectral_norm
 
 
 class ResidualBlock(nn.Module):
@@ -67,12 +68,16 @@ class Discriminator(nn.Module):
     def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
         super(Discriminator, self).__init__()
         layers = []
-        layers.append(nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1))
+        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
+        spectral_norm(conv)
+        layers.append(conv)
         layers.append(nn.LeakyReLU(0.01))
 
         curr_dim = conv_dim
         for i in range(1, repeat_num):
-            layers.append(nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1))
+            conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
+            spectral_norm(conv)
+            layers.append(conv)
             layers.append(nn.LeakyReLU(0.01))
             curr_dim = curr_dim * 2
 
@@ -80,6 +85,8 @@ class Discriminator(nn.Module):
         self.main = nn.Sequential(*layers)
         self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
         self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        spectral_norm(self.conv1)
+        spectral_norm(self.conv2)
         
     def forward(self, x):
         h = self.main(x)
