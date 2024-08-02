64bdc51c8864151d8e92ba91bb661fdc3046f79a
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-02 16:34:36 -0500

    initial hinge loss implementation

--- solver.py
+++ solver.py
@@ -242,13 +242,13 @@ class Solver(object):
 
             # Compute loss with real images.
             out_src, out_cls = self.D(x_real)
-            d_loss_real = F.binary_cross_entropy_with_logits(out_src, torch.ones_like(out_src))
+            d_loss_real = -min(0, out_src.mean()-1)
             d_loss_cls = self.classification_loss(out_cls, label_org, self.dataset)
 
             # Compute loss with fake images.
             x_fake = self.G(x_real, c_trg)
             out_src, out_cls = self.D(x_fake.detach())
-            d_loss_fake = F.binary_cross_entropy_with_logits(out_src, torch.zeros_like(out_src))
+            d_loss_fake = -min(0, -1 - out_src.mean())
 
             # Backward and optimize.
             d_loss = d_loss_real + d_loss_fake + self.lambda_cls * d_loss_cls
@@ -270,7 +270,7 @@ class Solver(object):
                 # Original-to-target domain.
                 x_fake = self.G(x_real, c_trg)
                 out_src, out_cls = self.D(x_fake)
-                g_loss_fake = F.binary_cross_entropy_with_logits(out_src, torch.ones_like(out_src))
+                g_loss_fake = -out_src
                 g_loss_cls = self.classification_loss(out_cls, label_trg, self.dataset)
 
                 # Target-to-original domain.
