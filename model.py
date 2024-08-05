23e1135a8441f63c7fa130247ccca2eae25f1337
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-05 15:07:09 -0500

    Fix.

--- model.py
+++ model.py
@@ -62,7 +62,7 @@ class Generator(nn.Module):
             curr_dim = curr_dim // 2
             self.ups.append(up)
 
-        self.final = nn.Conv2d(curr_dim, 3 * (poly_degree+1), kernel_size=7, stride=1, padding=3, bias=True)
+        self.final = nn.Conv2d(curr_dim // 2, 3 * (poly_degree+1), kernel_size=7, stride=1, padding=3, bias=True)
 
     def forward(self, im, c):
         # Replicate spatially and concatenate domain information.
