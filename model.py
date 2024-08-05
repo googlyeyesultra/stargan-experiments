f5801268459dd86b28e54dbab63c098c85eba3ae
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-05 12:20:22 -0500

    Drop in vision transformer for discriminator.

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.nn.utils.parametrizations import spectral_norm
+from torchvision import models
 
 
 class ResidualBlock(nn.Module):
@@ -75,31 +76,12 @@ class Generator(nn.Module):
 
 class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
-    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
+    def __init__(self, image_size=128, c_dim=5):
         super(Discriminator, self).__init__()
-        layers = []
-        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
-        spectral_norm(conv)
-        layers.append(conv)
-        layers.append(nn.LeakyReLU(0.01))
-
-        curr_dim = conv_dim
-        for i in range(1, repeat_num):
-            conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
-            spectral_norm(conv)
-            layers.append(conv)
-            layers.append(nn.LeakyReLU(0.01))
-            curr_dim = curr_dim * 2
-
-        kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
-        spectral_norm(self.conv1)
-        spectral_norm(self.conv2)
+        self.model = models.vision_transformer.vit_b_16(image_size=image_size, num_classes=c_dim+1)
         
     def forward(self, x):
-        h = self.main(x)
-        out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        h = self.model(x)
+        out_src = h[:,0]
+        out_cls = h[:,1:]
+        return out_src, out_cls
--- solver.py
+++ solver.py
@@ -72,7 +72,7 @@ class Solver(object):
         """Create a generator and a discriminator."""
         if self.dataset in ['CelebA', 'RaFD']:
             self.G = Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num)
-            self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim, self.d_repeat_num) 
+            self.D = Discriminator(self.image_size, self.c_dim) 
         elif self.dataset in ['Both']:
             self.G = Generator(self.g_conv_dim, self.c_dim+self.c2_dim+2, self.g_repeat_num)   # 2 for mask vector.
             self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim+self.c2_dim, self.d_repeat_num)
