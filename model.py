dc514fe65e3b52c409be412168921798a568bf31
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-08 13:33:52 -0500

    Increase poly epsilon dramatically, hopefully allowing constant term to more accurately represent any specific color.

--- model.py
+++ model.py
@@ -22,7 +22,7 @@ class ResidualBlock(nn.Module):
 
 class Generator(nn.Module):
     """Generator network."""
-    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6, poly_degree=3, poly_eps=.01):
+    def __init__(self, conv_dim=64, c_dim=5, repeat_num=6, poly_degree=3, poly_eps=10):
         super(Generator, self).__init__()
         
         self.poly_degree = poly_degree
