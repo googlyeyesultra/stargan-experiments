2b8935ff0be6d5554b71666efaba8aa3c0455931
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-12 11:32:07 -0500

    New discriminator architecture.

--- model.py
+++ model.py
@@ -68,33 +68,63 @@ class Generator(nn.Module):
         
         return slope * im + intercept
 
+
+class DiscrimBlock(nn.Module):
+    def __init__(self, conv_dim, num_convs=3):
+        super().__init__()
+        self.layers = nn.Sequential()
+        for i in range(num_convs):
+            conv = nn.Conv2d(conv_dim, conv_dim, kernel_size=3, stride=1, padding=1, bias=True)
+            spectral_norm(conv)
+            self.layers.append(conv)
+            if i != num_convs - 1:
+                self.layers.append(nn.LeakyReLU(.01, inplace=True))
+        
+        self.final_activ = nn.LeakyReLU(.01)
+        
+    def forward(self, x):
+        return self.final_activ(self.layers(x) + x)
+
+
+class DownsampleBlock(nn.Module):
+    def __init__(self, conv_dim, num_convs=3):
+        super().__init__()
+        self.layers = nn.Sequential()
+        for i in range(num_convs):
+            conv = nn.Conv2d(conv_dim, conv_dim, kernel_size=3, stride=1, padding=1, bias=True)
+            spectral_norm(conv)
+            self.layers.append(conv)
+            if i != num_convs - 1:
+                self.layers.append(nn.LeakyReLU(.01, inplace=True))
+        
+        self.final = nn.Sequential()
+        conv = nn.Conv2d(conv_dim, conv_dim, kernel_size=4, stride=2, padding=1, bias=True)
+        spectral_norm(conv)
+        self.final.append(conv)
+        self.final.append(nn.LeakyReLU(.01))
+        
+    def forward(self, x):
+        return self.final(self.layers(x) + x)
+
+
 class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
+    # Leaving repeat num in for now so I don't need to change solver/main.
     def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
-        super(Discriminator, self).__init__()
-        layers = []
-        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
-        spectral_norm(conv)
-        layers.append(conv)
-        layers.append(nn.LeakyReLU(0.01))
-
-        curr_dim = conv_dim
-        for i in range(1, repeat_num):
-            conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
-            spectral_norm(conv)
-            layers.append(conv)
-            layers.append(nn.LeakyReLU(0.01))
-            curr_dim = curr_dim * 2
-
-        kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        super().__init__()
+        self.layers = nn.Sequential()
+        size = image_size
+        while size > 1:
+            self.layers.append(DiscrimBlock(conv_dim))
+            self.layers.append(DiscrimBlock(conv_dim))
+            self.layers.append(DownsampleBlock(conv_dim))
+            size //= 2
+            
+        self.conv1 = nn.Conv2d(conv_dim, 1, kernel_size=1, bias=True)
+        self.conv2 = nn.Conv2d(conv_dim, c_dim, kernel_size=1, bias=True)
         spectral_norm(self.conv1)
         spectral_norm(self.conv2)
         
     def forward(self, x):
-        h = self.main(x)
-        out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        x = self.layers(x)
+        return self.conv1(x), self.conv2(x)
