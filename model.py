1df08d6ba5c64ec5b7be0179bc5e582c3e12b509
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-13 11:51:23 -0500

    Original discriminator but with separate tails for real/fake and each class.

--- model.py
+++ model.py
@@ -72,29 +72,40 @@ class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
     def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
         super(Discriminator, self).__init__()
-        layers = []
-        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
+        self.c_dim = c_dim
+        self.layers = nn.Sequential()
+        conv = nn.Conv2d(3, conv_dim, kernel_size=3, stride=1, padding=1)
         spectral_norm(conv)
-        layers.append(conv)
-        layers.append(nn.LeakyReLU(0.01))
+        self.layers.append(conv)
+        self.layers.append(nn.LeakyReLU(0.01))
 
         curr_dim = conv_dim
-        for i in range(1, repeat_num):
+        for i in range(repeat_num - repeat_num // 2):
             conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
             spectral_norm(conv)
-            layers.append(conv)
-            layers.append(nn.LeakyReLU(0.01))
+            self.layers.append(conv)
+            self.layers.append(nn.LeakyReLU(0.01))
             curr_dim = curr_dim * 2
-
-        kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
-        spectral_norm(self.conv1)
-        spectral_norm(self.conv2)
+            
+            
+        final_size = int(image_size / np.power(2, repeat_num))
+        self.tails = nn.ModuleList()
+        for t in range(c_dim + 1):
+            tail = nn.Sequential()
+            self.tails.append(tail)
+            for i in range(repeat_num // 2):
+                conv = nn.Conv2d(curr_dim, curr_dim, kernel_size=4, stride=2, padding=1)
+                spectral_norm(conv)
+                tail.append(conv)
+                tail.append(nn.LeakyReLU(0.01))
+        
+            final_conv = nn.Conv2d(curr_dim, 1, kernel_size=final_size, stride=1, padding=0, bias=False)
+            spectral_norm(final_conv)
+            tail.append(final_conv)
+            
         
     def forward(self, x):
-        h = self.main(x)
-        out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        x = self.layers(x)
+        out_src = self.tails[-1](x).squeeze((1, 2, 3))
+        out_cls = torch.stack([self.tails[i](x).squeeze((1, 2, 3)) for i in range(self.c_dim)], dim=1)
+        return out_src, out_cls
