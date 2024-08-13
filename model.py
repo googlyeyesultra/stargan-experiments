0f9eb1041da1cec94b9ba0d489e48945a464ea92
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-13 11:44:59 -0500

    Removed old assert.

--- model.py
+++ model.py
@@ -77,7 +77,6 @@ class Discriminator(nn.Module):
     """Discriminator network with PatchGAN."""
     def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
         super(Discriminator, self).__init__()
-        assert not repeat_num % 2
         self.c_dim = c_dim
         self.layers = nn.Sequential()
         conv = nn.Conv2d(3, conv_dim, kernel_size=3, stride=1, padding=1)
