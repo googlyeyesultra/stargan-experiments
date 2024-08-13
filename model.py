2b361c006ecb486f6438249dad7abd23d2cf65d7
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-13 11:36:27 -0500

    Fix.

--- model.py
+++ model.py
@@ -80,10 +80,6 @@ class Discriminator(nn.Module):
         assert not repeat_num % 2
         self.c_dim = c_dim
         self.layers = nn.Sequential()
-        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
-        spectral_norm(conv)
-        self.layers.append(conv)
-        self.layers.append(nn.LeakyReLU(0.01))
 
         curr_dim = conv_dim
         for i in range(repeat_num // 2):
