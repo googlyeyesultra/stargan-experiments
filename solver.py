05a19acfab98e14237c67120ca3763060785b9fa
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-14 15:49:57 -0500

    Mismatched d and g loss.

--- solver.py
+++ solver.py
@@ -229,13 +229,13 @@ class Solver(object):
 
             # Compute loss with real images.
             out_src, out_cls = self.D(x_real)
-            d_loss_real = -min(0, out_src.mean()-1)
+            d_loss_real = self.classification_loss(out_src, torch.ones_like(out_src))
             d_loss_cls = self.classification_loss(out_cls, label_org, self.dataset)
 
             # Compute loss with fake images.
             x_fake = self.G(x_real, c_trg)
             out_src, out_cls = self.D(x_fake.detach())
-            d_loss_fake = -min(0, -1 - out_src.mean())
+            d_loss_fake = self.classification_loss(out_src, torch.zeros_like(out_src))
 
             # Backward and optimize.
             d_loss = d_loss_real + d_loss_fake + self.lambda_cls * d_loss_cls
