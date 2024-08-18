d1a2070e28016f6ad4ced7846368948010315a6c
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-17 21:39:02 -0500

    v2 discrim adapted to fit in, SN

--- model.py
+++ model.py
@@ -3,6 +3,7 @@ import torch.nn as nn
 import torch.nn.functional as F
 import numpy as np
 from torch.nn.utils.parametrizations import spectral_norm
+import math
 
 
 class ResidualBlock(nn.Module):
@@ -73,33 +74,83 @@ class Generator(nn.Module):
         return (pows * coeffs).sum(1)
 
 
-class Discriminator(nn.Module):
-    """Discriminator network with PatchGAN."""
-    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
-        super(Discriminator, self).__init__()
-        layers = []
-        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
-        spectral_norm(conv)
-        layers.append(conv)
-        layers.append(nn.LeakyReLU(0.01))
+# https://github.com/clovaai/stargan-v2/blob/master/core/model.py
+class ResBlk(nn.Module):
+    def __init__(self, dim_in, dim_out, actv=nn.LeakyReLU(0.2),
+                 normalize=False, downsample=False):
+        super().__init__()
+        self.actv = actv
+        self.normalize = normalize
+        self.downsample = downsample
+        self.learned_sc = dim_in != dim_out
+        self._build_weights(dim_in, dim_out)
 
-        curr_dim = conv_dim
-        for i in range(1, repeat_num):
-            conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
-            spectral_norm(conv)
-            layers.append(conv)
-            layers.append(nn.LeakyReLU(0.01))
-            curr_dim = curr_dim * 2
-
-        kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+    def _build_weights(self, dim_in, dim_out):
+        self.conv1 = nn.Conv2d(dim_in, dim_in, 3, 1, 1)
+        self.conv2 = nn.Conv2d(dim_in, dim_out, 3, 1, 1)
         spectral_norm(self.conv1)
         spectral_norm(self.conv2)
-        
+        if self.normalize:
+            self.norm1 = nn.InstanceNorm2d(dim_in, affine=True)
+            self.norm2 = nn.InstanceNorm2d(dim_in, affine=True)
+        if self.learned_sc:
+            self.conv1x1 = nn.Conv2d(dim_in, dim_out, 1, 1, 0, bias=False)
+            spectral_norm(self.conv1x1)
+
+    def _shortcut(self, x):
+        if self.learned_sc:
+            x = self.conv1x1(x)
+        if self.downsample:
+            x = F.avg_pool2d(x, 2)
+        return x
+
+    def _residual(self, x):
+        if self.normalize:
+            x = self.norm1(x)
+        x = self.actv(x)
+        x = self.conv1(x)
+        if self.downsample:
+            x = F.avg_pool2d(x, 2)
+        if self.normalize:
+            x = self.norm2(x)
+        x = self.actv(x)
+        x = self.conv2(x)
+        return x
+
     def forward(self, x):
-        h = self.main(x)
-        out_src = self.conv1(h)
-        out_cls = self.conv2(h)
-        return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
+        x = self._shortcut(x) + self._residual(x)
+        return x / math.sqrt(2)  # unit variance
+
+class Discriminator(nn.Module):
+    def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
+        super().__init__()
+        dim_in = 2**14 // image_size
+        max_conv_dim=512
+        blocks = []
+        conv = nn.Conv2d(3, dim_in, 3, 1, 1)
+        spectral_norm(conv)
+        blocks += [conv]
+    
+        repeat_num = int(np.log2(image_size)) - 2
+        for _ in range(repeat_num):
+            dim_out = min(dim_in*2, max_conv_dim)
+            blocks += [ResBlk(dim_in, dim_out, downsample=True)]
+            dim_in = dim_out
+    
+        blocks += [nn.LeakyReLU(0.2)]
+        conv = nn.Conv2d(dim_out, dim_out, 4, 1, 0)
+        spectral_norm(conv)
+        blocks += [conv]
+        blocks += [nn.LeakyReLU(0.2)]
+        conv = nn.Conv2d(dim_out, c_dim+1, 1, 1, 0)
+        spectral_norm(conv)
+        blocks += [conv]
+        blocks += [conv]
+        
+        self.main = nn.Sequential(*blocks)
+
+    def forward(self, x):
+        out = self.main(x)
+        out_src = out[:,0,0,0]
+        out_cls = out[:,1:,0,0]
+        return out_src, out_cls
