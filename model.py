b68c1bc8eea33cc40f1728ebfcaa2ec65338bdb7
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-18 14:58:38 -0500

    Ideas with squeeze and excitation based discrim.

--- model.py
+++ model.py
@@ -73,33 +73,58 @@ class Generator(nn.Module):
         return (pows * coeffs).sum(1)
 
 
+class SEBlock(nn.Module):
+    def __init__(self, size, channels, downsample=False):
+        super().__init__()
+        self.size = size
+        self.ff = nn.Sequential()
+        self.ff.append(nn.Linear(channels, channels))
+        self.ff.append(nn.LeakyReLU(.1))
+        self.ff.append(nn.Linear(channels, channels))
+        self.ff.append(nn.Sigmoid())
+        
+        self.convnet = nn.Sequential()
+        conv1 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1)
+        spectral_norm(conv1)
+        self.convnet.append(conv1)
+        self.convnet.append(nn.LeakyReLU(.1))
+        if downsample:
+            conv2 = nn.Conv2d(channels, channels, kernel_size=4, stride=2, padding=1)
+        else:
+            conv2 = nn.Conv2d(channels, channels, kernel_size=3, stride=1, padding=1)
+        
+        spectral_norm(conv2)
+        self.convnet.append(conv2)
+
+    
+    def forward(self, x):
+        squeezed = F.avg_pool2d(x, self.size).squeeze((2, 3))
+        squeezed = self.ff(squeezed)
+        return x + self.convnet(x) * squeezed
+
 class Discriminator(nn.Module):
-    """Discriminator network with PatchGAN."""
     def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
-        super(Discriminator, self).__init__()
-        layers = []
-        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
-        spectral_norm(conv)
-        layers.append(conv)
-        layers.append(nn.LeakyReLU(0.01))
-
-        curr_dim = conv_dim
-        for i in range(1, repeat_num):
-            conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
-            spectral_norm(conv)
-            layers.append(conv)
-            layers.append(nn.LeakyReLU(0.01))
-            curr_dim = curr_dim * 2
-
+        super().__init__()
+        conv_dim = 128  # Just hacking this in since we're using conv_dim differently.
+        
+        self.layers = nn.Sequential()
+        initial = nn.Conv2d(3, conv_dim, kernel_size=3, stride=1, padding=1)
+        spectral_norm(self.initial)
+        self.layers.append(initial)
+        size = image_size
+        for i in range(repeat_num):
+            self.blocks.append(SEBlock(size, conv_dim))
+            self.blocks.append(SEBlock(size, conv_dim, True))
+            size //=2
+            
         kernel_size = int(image_size / np.power(2, repeat_num))
-        self.main = nn.Sequential(*layers)
-        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
-        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
+        self.conv1 = nn.Conv2d(conv_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(conv_dim, c_dim, kernel_size=kernel_size, bias=False)
         spectral_norm(self.conv1)
         spectral_norm(self.conv2)
         
     def forward(self, x):
-        h = self.main(x)
+        h = self.layers(x)
         out_src = self.conv1(h)
         out_cls = self.conv2(h)
         return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
