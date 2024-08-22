059541f2eacb899cd37ffb0e4ceec3d34daa54f6
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-21 19:13:11 -0500

    original discrim with sn

--- model.py
+++ model.py
@@ -89,31 +89,32 @@ class Generator(nn.Module):
         return x
 
 class Discriminator(nn.Module):
+    """Discriminator network with PatchGAN."""
     def __init__(self, image_size=128, conv_dim=64, c_dim=5, repeat_num=6):
-        super().__init__()
-        
-        conv_dim = 128  # Just hacking it here.
-        self.layers = nn.Sequential()
-        conv = nn.Conv2d(3, conv_dim, kernel_size=3, stride=1, padding=1)
+        super(Discriminator, self).__init__()
+        layers = []
+        conv = nn.Conv2d(3, conv_dim, kernel_size=4, stride=2, padding=1)
         spectral_norm(conv)
-        self.layers.append(conv)
+        layers.append(conv)
+        layers.append(nn.LeakyReLU(0.01))
 
-        down_layers = int(math.log2(image_size))
+        curr_dim = conv_dim
+        for i in range(1, repeat_num):
+            conv = nn.Conv2d(curr_dim, curr_dim*2, kernel_size=4, stride=2, padding=1)
+            spectral_norm(conv)
+            layers.append(conv)
+            layers.append(nn.LeakyReLU(0.01))
+            curr_dim = curr_dim * 2
 
-        for i in range(down_layers):
-            self.layers.append(Block(conv_dim, sn=True, updown="n"))
-            self.layers.append(Block(conv_dim, sn=True, updown="d"))
-
-        for i in range(3):
-            self.layers.append(Block(conv_dim, sn=True, updown="n"))     
-
-        self.conv1 = nn.Conv2d(conv_dim, 1, kernel_size=1, stride=1, padding=0, bias=False)
-        self.conv2 = nn.Conv2d(conv_dim, c_dim, kernel_size=1, stride=1, padding=0, bias=False)
+        kernel_size = int(image_size / np.power(2, repeat_num))
+        self.main = nn.Sequential(*layers)
+        self.conv1 = nn.Conv2d(curr_dim, 1, kernel_size=3, stride=1, padding=1, bias=False)
+        self.conv2 = nn.Conv2d(curr_dim, c_dim, kernel_size=kernel_size, bias=False)
         spectral_norm(self.conv1)
         spectral_norm(self.conv2)
         
     def forward(self, x):
-        h = self.layers(x)
+        h = self.main(x)
         out_src = self.conv1(h)
         out_cls = self.conv2(h)
         return out_src, out_cls.view(out_cls.size(0), out_cls.size(1))
