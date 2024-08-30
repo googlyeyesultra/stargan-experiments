cd20b5790d5ba9cfb6c465649bb2b4a246fe457f
googlyeyesultra <googlyeyesultra@gmail.com>
2024-08-30 09:57:14 -0500

    no weight norm on last in gen

--- model.py
+++ model.py
@@ -56,7 +56,6 @@ class Generator(nn.Module):
             curr_dim = curr_dim // 2
 
         c = nn.Conv2d(curr_dim, 3, kernel_size=7, stride=1, padding=3)
-        weight_norm(c)
         self.layers.append(c)
         self.layers.append(nn.Tanh())
 
