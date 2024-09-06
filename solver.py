acae081a5d5932ab62c0d1c5751471715c70c29b
googlyeyesultra <googlyeyesultra@gmail.com>
2024-09-06 09:49:23 -0500

    3 generators

--- solver.py
+++ solver.py
@@ -71,7 +71,7 @@ class Solver(object):
     def build_model(self):
         """Create a generator and a discriminator."""
         if self.dataset in ['CelebA', 'RaFD']:
-            self.Gs = [Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num) for i in range(5)]
+            self.Gs = [Generator(self.g_conv_dim, self.c_dim, self.g_repeat_num) for i in range(3)]
             self.D = Discriminator(self.image_size, self.d_conv_dim, self.c_dim, self.d_repeat_num) 
         elif self.dataset in ['Both']:
             self.G = Generator(self.g_conv_dim, self.c_dim+self.c2_dim+2, self.g_repeat_num)   # 2 for mask vector.
