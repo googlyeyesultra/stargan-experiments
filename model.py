da1fe8c721ff85c43ba225b531c672b1c742ea88
googlyeyesultra <googlyeyesultra@gmail.com>
2024-09-21 13:06:58 -0500

    fix

--- model.py
+++ model.py
@@ -21,7 +21,7 @@ class ModConv(nn.Module):  # Modulated convolution like StyleGAN 2.
         
         self.weight = nn.Parameter(torch.randn(1, out_channel, in_channel, kernel_size, kernel_size))
         self.modulation = nn.Linear(style_dim, in_channel)
-        self.bias = nn.Parameter(torch.randn(1, out_channel, 1, 1))
+        self.bias = nn.Parameter(torch.zeros(1, out_channel, 1, 1))
         
         # weight_norm(self.weight)  # Can't weight norm this
         weight_norm(self.modulation)
