d3e25bad135bfbfba902b7b0e10133e31a8c22c8
googlyeyesultra <googlyeyesultra@gmail.com>
2024-09-26 11:24:02 -0500

    cycle consistency instead of auto loss

--- solver.py
+++ solver.py
@@ -258,7 +258,7 @@ class Solver(object):
                 g_loss_fake = -out_src.mean()
 
                 # Identity loss (instead of cycle consistency).
-                x_reconst = self.G(x_real, c_org, c_org)
+                x_reconst = self.G(x_fake, c_org, c_trg)
                 g_loss_rec = torch.mean(torch.abs(x_real - x_reconst))
 
                 # Backward and optimize.
